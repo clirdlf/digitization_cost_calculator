ee491c9d90952b3c2810598e79f328a97b180bc4
Wayne Graham <wgraham@clir.org>
2016-09-12 10:57:19 -0400

    updates for javascript

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,51 @@
-(function() {
-  'use strict';
+export let digitizationCalculator = (form) => {
+  return {form};
+};
 
-  // The actual calculator logic
-})();
+export default class DigitizationCalculator {
+  constructor() {
+    
+  }
+
+  get studentLabor() {
+
+  }
+
+  get staffLabor() {
+
+  }
+}
+
+
+//var digitizationCalculator = (function () {
+  //'use strict';
+
+  //// The actual calculator logic in vanilla JS
+
+  //var calcuations = {
+    //extent:                         0,
+    //salary:                         0,
+    //salary_benefits:                0,
+    //hourly:                         0,
+    //hourly_benefits:                0,
+    //condition_review_percentage:    0,
+    //condition_review_by:            '',
+    //disbinding_percentage:          0,
+    //disbinding_by:                  0,
+    //fastener_removal_percentage:    0,
+    //fastener_remval_by:             '',
+    //capture_device:                 '',
+    //capture_level:                  0,
+    //alignment_percentage:           0,
+    //alignment_percentage_by:        '',
+    //background_removal_percentage:  0,
+    //background_removal_by:          '',
+
+    //getVariables: function() {
+      //return "hi";
+    //},
+
+  //}
+
+
+//})();
--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 (function($) {
   'use strict';
 
+  //import * as calculator from './calculator';
+
   $(document).ready(function() {
     // app and form logic
-    console.log('working');
 
-    var max_fields = 10;
     var wrapper    = $('.form-group');
     var add_button = $('.add_field'); // add salary? add_wage?
 
@@ -15,13 +15,22 @@
       var field = t.siblings('label').attr('for');
       var clean_field = field.replace(/\[\]/, '');
       // TODO: finish off making this cleaner
-      t.parent().append(`<div class="form-group"><label for="salary">${clean_field.toProperCase()}</label> <input name="${field}" type="number" step="any" placeholder="Annual Salary"> <input name="${field}-benefits[]" type="number" step="any" placeholder="% Benefits"> <a class="remove_field">X</a></div>`);
+      t.parent().append(`<div class="form-group"><label for="${clean_field}">${clean_field.toProperCase()}</label> <input name="${field}" type="number" step="any" placeholder="Annual Salary"> <input name="${field}-benefits[]" type="number" step="any" placeholder="% Benefits"> <a class="remove_field">X</a></div>`);
     });
 
     $(wrapper).on('click', '.remove_field', function(e) {
       e.preventDefault();
       $(this).parent('div').remove();
-      //count--;
+    });
+
+    // handle form submission
+    $('form#calculator').submit(function(e) {
+      e.preventDefault();
+      console.log($(this).serialize());
+
+      console.log(digitizationCalculator('hi'));
+      //var calculator = digitizationCalculator.getVariables();
+      //console.table($(this).serializeArray());
     });
 
   });
