e3ef20460a4fa61b653a373d6d6fee5b630dcee4
Wayne Graham <wgraham@clir.org>
2016-09-12 15:53:24 -0400

    Update to use baseurl for github pages
    Fixes asset paths getting loaded for github pages
    
    closes #1

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ var messages = {
  */
 gulp.task('jekyll-build', function (done) {
     browserSync.notify(messages.jekyllBuild);
-    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+    return cp.spawn( jekyll , ['build', '--baseurl', ''], {stdio: 'inherit'})
         .on('close', done);
 });
 
