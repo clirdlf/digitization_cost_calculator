40ce4f016016082e5935945496a78043c9fa5479
Wayne Graham <wgraham@clir.org>
2016-10-24 18:01:46 -0400

    Add header/footer/about
    Adds header
    Adds Footer
    adds about page
    Adds styles for components

--- js/utilities.js
+++ js/utilities.js
@@ -9,6 +9,20 @@ var dlf = [
 
 console.log(dlf);
 
+/** Simple spam protection for email addresses using jQuery.
+ * Well, the protection isn’t jQuery-based, but you get the idea.
+ * This snippet allows you to slightly ‘obfuscate’ email addresses to make it harder for spambots to harvest them, while still offering a readable address to your visitors.
+ * E.g.
+ * <a href="mailto:foo(at)example(dot)com">foo at example dot com</a>
+ * →
+ * <a href="mailto:foo@example.com">foo@example.com</a>
+ */
+ $('a[href^="mailto:"]').each(function() {
+   this.href = this.href.replace('(at)', '@').replace(/\(dot\)/g, '.');
+   // Remove this line if you don't want to set the email address as link text:
+   this.innerHTML = this.href.replace('mailto:', '');
+ });
+
 // @see http://stackoverflow.com/questions/45888/what-is-the-most-efficient-way-to-sort-an-html-selects-options-by-value-while
 // TODO: only sort things that have value in the select box; may be better to do in the Rakefile...
 $.fn.sort_select_box = function(){
