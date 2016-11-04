a1bd81949423f74ce4c8c9d1cec9da1276108a9d
Wayne Graham <wgraham@clir.org>
2016-11-04 08:00:27 -0400

    Reformat benefits percentage
    closes #16

--- js/main_refactor.js
+++ js/main_refactor.js
@@ -70,7 +70,7 @@ people.push(empty_person);
             div += '<span class="name"><strong>' + this.name + '</strong></span> ';
             div += '<span class="rate_type">' + this.type + '</span> ';
             div += '<span class="rate">$' + this.set_rate + '</span> ';
-            div += '<span class="benefits">' + this.benefits_percent + '%</span> ';
+            div += '<span class="benefits">' + (this.benefits_percent * 100) + '%</span> ';
             div += '</p></div>';
             html += div;
         });
